714c172a9a81ffcbb5ffd94319d8651c866c5e14
Ivan Wills <Ivan.Wills2@optus.com.au>
2020-09-09 12:15:08 +1000

    Adding the index.js file

---
+++ index.js
@@ -0,0 +1,11 @@
+const Bamboo = require('./src/Bamboo');
+const Project = require('./src/Project');
+const Plan = require('./src/Plan');
+const Build = require('./src/Build');
+
+modules.exports = {
+  Bamboo,
+  Project,
+  Plan,
+  Build,
+};
